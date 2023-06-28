1a4d856230f1853070531184397f22bb1aba9d51
NewtonsMama <93648886+NewtonsMama@users.noreply.github.com>
2023-06-29 02:50:58 +0530

    Update config.py (change train/val dir)

--- CycleGAN/config.py
+++ CycleGAN/config.py
@@ -3,8 +3,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "/kaggle/input/horse-zebra/data/train"
-VAL_DIR = "/kaggle/input/horse-zebra/data/val"
+TRAIN_DIR = "/kaggle/input/ct-to-mri-cgan/Dataset/images"
+VAL_DIR = "/kaggle/input/ct-to-mri-cgan/Dataset/images"
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
