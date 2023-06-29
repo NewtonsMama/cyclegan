a181aabfb0010d9ef5175da1b99f424d5f008282
NewtonsMama <93648886+NewtonsMama@users.noreply.github.com>
2023-06-29 21:04:01 +0530

    Update config.py (new dir)

--- CycleGAN/config.py
+++ CycleGAN/config.py
@@ -1,15 +1,18 @@
 import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+#import torchvision.transforms as T
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "/kaggle/input/ct-to-mri-cgan/Dataset/images"
-VAL_DIR = "/kaggle/input/ct-to-mri-cgan/Dataset/images"
+TRAIN_A_DIR = "/kaggle/input/mri-ct-processed/processed_images/train/trainA"
+TRAIN_B_DIR = "/kaggle/input/mri-ct-processed/processed_images/train/trainB"
+VAL_A_DIR = "/kaggle/input/mri-ct-processed/processed_images/val/testA"
+VAL_B_DIR = "/kaggle/input/mri-ct-processed/processed_images/val/testB"
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
 LAMBDA_CYCLE = 10
-NUM_WORKERS = 2
+NUM_WORKERS = 0
 NUM_EPOCHS = 2
 LOAD_MODEL = False
 SAVE_MODEL = True
@@ -17,12 +20,13 @@ CHECKPOINT_GEN_H = "genh.pth.tar"
 CHECKPOINT_GEN_Z = "genz.pth.tar"
 CHECKPOINT_CRITIC_H = "critich.pth.tar"
 CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
+SAVE_IMG_TRAIN_DIR = "/kaggle/working/cyclegan/CycleGAN/saved_images"
 
 transforms = A.Compose(
     [
-        A.Resize(width=256, height=256),
-        A.HorizontalFlip(p=0.5),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
+        #A.Resize(width=256, height=256),
+        #A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
+        A.Normalize(mean=[0.5], std=[0.5], max_pixel_value=255),
         ToTensorV2(),
     ],
     additional_targets={"image0": "image"},
