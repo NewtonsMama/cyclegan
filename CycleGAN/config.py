b3c5c629b547eb5e078661e524a6d2ded08fdd18
NewtonsMama <93648886+NewtonsMama@users.noreply.github.com>
2023-07-01 02:12:28 +0530

    Update config.py (no resize)

--- CycleGAN/config.py
+++ CycleGAN/config.py
@@ -25,7 +25,7 @@ SAVE_IMG_TRAIN_DIR = "/kaggle/working/cyclegan/CycleGAN/saved_images"
 
 transforms = A.Compose(
     [
-        A.Resize(width=64, height=64),
+        #A.Resize(width=64, height=64),
         #A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
         A.Normalize(mean=[0.5], std=[0.5], max_pixel_value=255),
         ToTensorV2(),
